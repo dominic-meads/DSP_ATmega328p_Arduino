a915d137b3aeafddd9d99a80a835be0178a32e59
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2020-08-10 18:21:50 -0700

    Update MATLAB_interpolations.m

--- Final/MATLAB_interpolations.m
+++ Final/MATLAB_interpolations.m
@@ -1,35 +1,40 @@
-% Test signal was a 0.5V amplitude, 50 Hz sine wave with a 2.5V offset. Test
-% noise of 1.1 KHz was added to evaluate filter performance. 
+% Test signal was a 0.31V amplitude, 50 Hz sine wave with a 2.5V offset. Test
+% noise of 1.1 KHz (0.19V amplitude, 2.5V offset) was added to evaluate filter performance. 
 
-% The cuttoff frequency of the filter was 0.02*fs or 56 Hz. 
+% The cuttoff frequency of the filter was 0.02*fs or 56 Hz.
 
+n = 1:150;
+fs = 1513.77;  % 1/660.6us
+Ts = 1/fs;
+nTs = n*Ts;
+
+xin = -0.31*sin(50*2*pi*nTs + 1.5) + 0.19*sin(1100*2*pi*nTs) + 2.5;  % input signal (inverted and shifted to visually match up better with output samples)
+subplot(2,2,1); plot(n,xin); title('input signal'); axis([0 150 1.5 3.5]); xlabel('Samples'); ylabel('Voltage');
 
 % below are some output samples from the chip (taken using a logic analyzer)
-fID = fopen('logic_bus_output_kernel_length_17.txt', 'r');
+fID = fopen('FIR_output.txt', 'r');
 data = textscan(fID,'%s');
 fclose(fID);
-x = str2double(data{1}(1:300))';
+x = str2double(data{1}(1:150))';
 n = 0:length(x)-1;
 
-subplot(2,2,1); stem(n,x,'Linestyle','none'); title('output samples');
-subplot(2,2,2); stairs(n,x); title('ZOH');
-subplot(2,2,3); plot(n,x); title('FOH'); 
+subplot(2,2,2); stem(n,(x/1023)*5,'Linestyle','none'); title('output samples'); axis([0 150 1.5 3.5]); xlabel('Samples'); ylabel('Voltage');
+subplot(2,2,3); stairs(n,(x/1023)*5); title('ZOH'); axis([0 150 1.5 3.5]); xlabel('Samples'); ylabel('Voltage');
+subplot(2,2,4); plot(n,(x/1023)*5); title('FOH'); axis([0 150 1.5 3.5]); xlabel('Samples'); ylabel('Voltage');
 
-% The period is about 56 samples
+% The period is about 30 samples
 
-% Each sample on logic analyzer takes 383.7us, which gives a frequency of
-% about 47 Hz, very close to input 50 Hz.
+% Each output sample on logic analyzer has a period of 660.6us, which gives
+% a frequency of about 50.5 Hz, very close to input 50 Hz.
 
-% The max for a 10 bit output is 1024, input signal was 1V amplitude with
-% 2.5V offset. Looking at the figures, the offset is around 516 units, with
-% 516/1024 =~ 0.5. This is acceptable because 2.5V is half of the reference
-% voltage on the chip.
+% The max for a 10 bit output is 1023, input signal was 0.3V amplitude with
+% 2.5V offset. Looking at the figures, the output voltage offset is very
+% close to 2.5V
 
-% Max = 605 and min = 410, averaging a 97.5 unit swing, (98/1024)*5V = 0.5V
-% which is also acceptable because the input target signal swings between
-% 3V and 2V
+% Max = 2.854 and min = 2.175, averaging about a 0.34V amplitude.
+% (amplitude a bit higher than expected)
 
-% Visual simililarity to a sinusoid is ok. 
+% Visual simililarity to a sinusoid is good. 
 
 % Overall, both interpolations show acceptable analog reconstruction (the
 % code works!!!!!)
