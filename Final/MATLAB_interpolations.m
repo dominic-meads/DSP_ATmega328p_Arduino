e7e5cbec2b7855c88ec40d3a5df3320e5936925c
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2020-08-15 16:23:01 -0700

    update with new test stimulus

--- Final/MATLAB_interpolations.m
+++ Final/MATLAB_interpolations.m
@@ -1,15 +1,15 @@
-% Test signal was a 0.31V amplitude, 50 Hz sine wave with a 2.5V offset. Test
-% noise of 1.1 KHz (0.19V amplitude, 2.5V offset) was added to evaluate filter performance. 
+% Test signal was a 1V amplitude, 50 Hz sine wave with a 2.5V offset. Test
+% noise of 1.1 KHz (0.77V amplitude, 2.5V offset) was added to evaluate filter performance. 
 
-% The cuttoff frequency of the filter was 0.02*fs or 56 Hz.
+% The cuttoff frequency of the filter was 0.02*fs or 78.4 Hz.
 
 n = 1:150;
-fs = 1513.77;  % 1/660.6us
+fs = 2506.267;  % 1/399us
 Ts = 1/fs;
 nTs = n*Ts;
 
-xin = -0.31*sin(50*2*pi*nTs + 1.5) + 0.19*sin(1100*2*pi*nTs) + 2.5;  % input signal (inverted and shifted to visually match up better with output samples)
-subplot(2,2,1); plot(n,xin); title('input signal'); axis([0 150 1.5 3.5]); xlabel('Samples'); ylabel('Voltage');
+xin = -sin(50*2*pi*nTs - 0.2) + 0.77*sin(1100*2*pi*nTs) + 2.5;  % input signal (inverted and shifted to visually match up better with output samples)
+subplot(2,2,1); plot(nTs,xin); title('input signal'); axis([0 0.06 0 5]); xlabel('time (s)'); ylabel('Voltage');
 
 % below are some output samples from the chip (taken using a logic analyzer)
 fID = fopen('FIR_output.txt', 'r');
@@ -18,21 +18,21 @@ fclose(fID);
 x = str2double(data{1}(1:150))';
 n = 0:length(x)-1;
 
-subplot(2,2,2); stem(n,(x/1023)*5,'Linestyle','none'); title('output samples'); axis([0 150 1.5 3.5]); xlabel('Samples'); ylabel('Voltage');
-subplot(2,2,3); stairs(n,(x/1023)*5); title('ZOH'); axis([0 150 1.5 3.5]); xlabel('Samples'); ylabel('Voltage');
-subplot(2,2,4); plot(n,(x/1023)*5); title('FOH'); axis([0 150 1.5 3.5]); xlabel('Samples'); ylabel('Voltage');
+subplot(2,2,2); stem(n,(x/1023)*5,'Linestyle','none'); title('output samples'); axis([0 150 0 5]); xlabel('Samples'); ylabel('Voltage');
+subplot(2,2,3); stairs(n,(x/1023)*5); title('ZOH'); axis([0 150 0 5]); xlabel('Samples'); ylabel('Voltage');
+subplot(2,2,4); plot(n,(x/1023)*5); title('FOH'); axis([0 150 0 5]); xlabel('Samples'); ylabel('Voltage');
 
-% The period is about 30 samples
+% The period is about 50 samples
 
-% Each output sample on logic analyzer has a period of 660.6us, which gives
-% a frequency of about 50.5 Hz, very close to input 50 Hz.
+% Each output sample on logic analyzer has a period of 399us, which gives
+% a frequency of about 50.125 Hz, very close to input 50 Hz.
 
-% The max for a 10 bit output is 1023, input signal was 0.3V amplitude with
+% The max for a 10 bit output is 1023, input signal was 1V amplitude with
 % 2.5V offset. Looking at the figures, the output voltage offset is very
 % close to 2.5V
 
-% Max = 2.854 and min = 2.175, averaging about a 0.34V amplitude.
-% (amplitude a bit higher than expected)
+% Max = 3.358 and min = 1.495, averaging about a 0.93V amplitude.
+% (amplitude a bit lower than expected)
 
 % The below code shows the frequency response of the filter. (found by taking
 % the dtft of the impulse response (filter kernel).
